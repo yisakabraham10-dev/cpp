c14c7118b0999a446ead852531579c7cfb54a782
Yisak Abraham <yisakabraham10@gmail.com>
2026-04-18 19:46:41 +0300

    Initial commit

---
+++ mia.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "C++ is ready on Fedora!" << std::endl;
+    return 0;
+}
---
+++ shukaku.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Hello World !!!"<< endl;
+    int a = 200;
+    cout << a<< endl;
+    return 0;
+
+}
