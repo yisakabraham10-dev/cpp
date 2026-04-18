c2e1efb67d1475d08b6b8eb9b29ea34803c35f10
Yisak Abraham <yisakabraham10@gmail.com>
2026-04-18 21:07:39 +0300

    done with the room cpp

---
+++ room.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int room_width, room_length, room_area;
+    
+    cout<<"Enter the width of the room: ";
+    cin>> room_area;
+
+    cout<< "Enter the length of the room: ";
+    cin>>room_length;
+
+    room_area = room_area*room_length;
+    
+    cout<< "The room area is " << room_area<< endl;
+
+    return 0;
+}
