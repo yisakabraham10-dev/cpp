72b9eea6619938cf69887a238e445003b12cfcbf
Yisak Abraham <yisakabraham10@gmail.com>
2026-05-22 16:49:17 +0300

    may 22

--- A_Next_Round.cpp
+++ A_Next_Round.cpp
@@ -1,4 +1,3 @@
-//158a
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,6 +5,28 @@ int main (){
     ios:: sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, k;
+    cin>> n >> k;
+    int counter = 0;
+
+    int A[n];
     
+
+    for (int m =0; m<n; m++){
+        int x;
+        cin>>x;
+        A[m] = x;
+    }
+
+    for (int i = 0; i<n;  i++){
+        if (A[i] > k){
+            counter +=1;
+        }
+    }
+    cout<<counter;
+
+
+    
+
 
 }
