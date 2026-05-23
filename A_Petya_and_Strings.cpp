b566eb1b2e5fd516449f24a87f5977d2d9b13451
Yisak Abraham <yisakabraham10@gmail.com>
2026-05-23 22:32:47 +0300

    one more problem

--- A_Petya_and_Strings.cpp
+++ A_Petya_and_Strings.cpp
@@ -1,1 +1,17 @@
-??
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (int i=1; i<=2; i++){
+        string x, y;
+        int xlength , ylength;
+
+        cin>>x>>y;
+        
+        xlength = x.length(), ylength = y.length();
+        
+    }
+}
