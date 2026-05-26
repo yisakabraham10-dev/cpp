c27a5c490c1c247376eb243d0aea06284f37ba46
Yisak Abraham <yisakabraham10@gmail.com>
2026-05-26 19:57:16 +0300

    learning about cpp

---
+++ vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> x {1,2,3,4,5};
+
+    for (int i =0; i< x.size(); i++){
+        cout <<x[i];
+    }
+
+
+}
