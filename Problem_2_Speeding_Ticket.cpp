cba903a811fd7964cbd0e74fc9fea05304c00bf1
Yisak Abraham <yisakabraham10@gmail.com>
2026-06-06 11:38:47 +0300

    continuing from where we left off.

---
+++ Problem_2_Speeding_Ticket.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+ 
+int main(){
+    int M, N, counter=0;
+
+    for ()
+}
--- Problem_3_The_Cow_Signal.cpp
+++ Problem_3_The_Cow_Signal.cpp
@@ -8,6 +8,9 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    freopen("cowsignal.in", "r", stdin);
+    freopen("cowsignal.out", "w", stdout);
+
     int x, y, m;
 
     cin>>x>>y>>m;
@@ -20,11 +23,10 @@ int main(){
         for (int v =0; v<y; v++){
             new_row.append(m,row[v]);
         }
-    for(int i=0; i<m; i++){
+    for(int n=0; n<m; n++){
         cout<<new_row<<"\n";
     }
 
         
 
-    }
-}
+    }}
