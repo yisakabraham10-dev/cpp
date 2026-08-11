21c77e63b524c9ccb0c4dc01f40af82ddc94ce6a
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-11 10:40:29 +0300

    how should i proceed after this?

--- Problem_2_The_Bucket_List.cpp
+++ Problem_2_The_Bucket_List.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int main (){
+    int N; 
+    cin>> N;
+
+    vector<int> lower_time_interval;
+    vector<int> higher_time_interval;
+    vector<int> number_of_buckets;
+    vector<int> time;
+
+    for (int i = 0; i< N; i++){
+        int s;
+        int t;
+        int b;
+        cin>>s>>t>>b;
+
+        lower_time_interval.push_back(s);
+        higher_time_interval.push_back(t);
+        number_of_buckets.push_back(b);
+    }
+
+
+}
