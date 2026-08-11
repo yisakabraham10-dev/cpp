ea1a5f637d192ac3f124efa3c15e8f69b7cfd8a6
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-11 11:31:16 +0300

    not the most efficient answer but an answer nonetheless

--- Problem_2_The_Bucket_List.cpp
+++ Problem_2_The_Bucket_List.cpp
@@ -5,13 +5,16 @@
 using namespace std;
 
 int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int N; 
     cin>> N;
 
     vector<int> lower_time_interval;
     vector<int> higher_time_interval;
     vector<int> number_of_buckets;
-    vector<int> time;
+    vector<int> time(10000);
 
     for (int i = 0; i< N; i++){
         int s;
@@ -22,7 +25,14 @@ int main (){
         lower_time_interval.push_back(s);
         higher_time_interval.push_back(t);
         number_of_buckets.push_back(b);
+
+            for (int i=s; i<= t; i++){
+                time[i]+=b;
+            }
+
     }
 
-
+    int m = *max_element(time.begin(), time.end());
+    cout<<m;
+    
 }
