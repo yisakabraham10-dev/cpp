e127aceee1c55c9ed1f95eba720fd99f1b4a12e9
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-14 11:10:35 +0300

    understood what to do

--- Problem_3_Measuring_Traffic.cpp
+++ Problem_3_Measuring_Traffic.cpp
@@ -5,38 +5,27 @@
 
 using namespace std;
 
+struct traffic{
+    string type;
+    int low;
+    int high;
+};
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
+    
+    int N; 
     cin>>N;
 
-    vector<string> onoroff(N);
-    vector<int> lower_bound(N);
-    vector<int> upper_bound(N);
+    vector<traffic> traffics(N);
 
-    for (int i =0; i< N; i++){
-        cin>>onoroff[i];
-        cin>>lower_bound[i];
-        cin>>upper_bound[i];
+
+    for (int i = 0; i < N; i++){
+        cin>>traffics[i].type>>traffics[i].low>>traffics[i].high;
     }
 
-    int lu = *max_element(lower_bound.begin(), lower_bound.end());
-    int ll = *min_element(lower_bound.begin(), lower_bound.end());
-
-    int uu = *max_element(upper_bound.begin(), upper_bound.end());
-    int ul = *min_element(upper_bound.begin(), upper_bound.end());
-
-    int a,b,c,d;
-
-    for (int i = 0; i< N; i++){
-        if(onoroff[i] == "on"|| onoroff[i]=="false"){
-            continue;
-        }
-        if(onoroff[i] == "none"){
-            
-        }
-    }
+    
 
 }
