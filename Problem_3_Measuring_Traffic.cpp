1e69790657b3841cdc7732f3eda47dc0272e4442
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-14 15:32:33 +0300

    will finish this

--- Problem_3_Measuring_Traffic.cpp
+++ Problem_3_Measuring_Traffic.cpp
@@ -1,31 +1,52 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
-#include<string>
 #include<algorithm>
+#include<string>
+#include<climits>
 
 using namespace std;
 
 struct traffic{
-    string type;
-    int low;
-    int high;
+    string onoroff;
+    int lb;
+    int ub;
 };
 
-int main(){
+int main (){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
-    int N; 
+    int N;
     cin>>N;
 
     vector<traffic> traffics(N);
 
-
-    for (int i = 0; i < N; i++){
-        cin>>traffics[i].type>>traffics[i].low>>traffics[i].high;
+    for (int i = 0; i< N; i++){
+        cin>>traffics[i].onoroff>>traffics[i].lb>>traffics[i].ub;
     }
+int ub_increment= 0;
+int lb_increment = 0;
+int initial_assump_lb = INT_MIN;
+int initial_assump_ub = INT_MAX;
 
-    
+//for the inital stage
+    for (int i =0; i < N; i++){
+        if(traffics[i].onoroff == "on" ){
+            lb_increment -= traffics[i].lb;
+            ub_increment -= traffics[i].ub;
 
+        }
+        else if(traffics[i].onoroff== "off"){
+            lb_increment += traffics[i].lb;
+            ub_increment += traffics[i].ub;
+        }
+        else{
+            if (traffics[i].lb< initial_assump_lb){
+                initial_assump_lb = traffics[i].lb;
+            }
+            if (traffics[i].ub< initial_assump_ub){
+                initial_assump_ub= traffics[i].ub;
+            }
+        }
+    }
 }
