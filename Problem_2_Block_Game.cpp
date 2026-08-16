315d875b42db26d1ee4792ca2b547cd3fc2e0ccf
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-16 23:43:35 +0300

    will continue to do this

---
+++ Problem_2_Block_Game.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+struct Board{
+    string name1, name2;
+};
+struct letter_representation{
+    char alphabet; int amount;
+};
+
+int main (){
+    int N; cin>>N;
+
+    vector<Board> boards (N);
+    vector<char> alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n","o", "p", "q","r","s","t", "u", "v", "w", "x", "y", "z"};
+
+    for (int i = 0; i < N; i++){
+        cin>>boards[i].name1>>boards[i].name2;
+    }
+}
