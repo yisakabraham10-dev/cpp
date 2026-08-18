ddd43793e8830f23e2fea7a48a5850cb78a2a681
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-18 08:13:27 +0300

    done

--- Problem_3_Mowing_the_Field.cpp
+++ Problem_3_Mowing_the_Field.cpp
@@ -20,7 +20,9 @@ int main (){
     coordinates[0].x = 0;
     coordinates[0].y = 0;
 
-    for (int i =1; i< N; i++){
+    int current = 0;
+
+    for (int i =1; i<= N; i++){
 
         char a;
         cin>>a;
@@ -28,38 +30,36 @@ int main (){
         int b;
         cin>>b;
 
-        if (a == 'N'){
-            for (int j = 0; j<b; j++){
-                coordinates[i+j].x = coordinates[i+j-1].x;
-                coordinates[i+j].y = coordinates[i+j-1].y+1;
+        for (int j = 0; j<b; j++){
+
+            current++;
+            
+            if (a =='N'){
+                coordinates[current].x = coordinates[current-1].x;
+                coordinates[current].y = coordinates[current-1].y+1;                
             }
-        }
-        if (a == 'E'){
-            for (int j = 0; j<b; j++){
-                coordinates[i+j].x = coordinates[i+j-1].x+1;
-                coordinates[i+j].y = coordinates[i+j-1].y;
+
+            else if (a == 'E'){
+                coordinates[current].x = coordinates[current-1].x+1;
+                coordinates[current].y = coordinates[current-1].y;
             }
-        }
-        if (a == 'S'){
-            for (int j = 0; j<b; j++){
-                coordinates[i+j].x = coordinates[i+j-1].x;
-                coordinates[i+j].y = coordinates[i+j-1].y-1; 
+            else if (a == 'S'){
+                coordinates[current].x = coordinates[current-1].x;
+                coordinates[current].y = coordinates[current-1].y-1;
             }
-        }
-        if (a == 'W'){
-            for (int j = 0; j<b; j++){
-                coordinates[i+j].x = coordinates[i+j-1].x-1;
-                coordinates[i+j].y = coordinates[i+j-1].y;
+            else if (a == 'W'){
+                coordinates[current].x = coordinates[current-1].x-1;
+                coordinates[current].y = coordinates[current-1].y;
             }
         }
     }
     // check the max
-    int moka = INT_MIN;
-    for (int i = 0; i < coordinates.size(); i++){
-        for (int j = i; j <coordinates.size(); j++){
+    int moka = INT_MAX;
+    for (int i = 0; i <= current; i++){
+        for (int j = i+1; j <=current; j++){
             if ((coordinates[i].x==coordinates[j].x)&&(coordinates[i].y==coordinates[j].y)){
                 int no = j -i;
-                moka = max(moka, no);
+                moka = min(moka, no);
             }
         }
     }
