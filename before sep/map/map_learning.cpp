1d4dd0795a210d2c54f69b4c1073b21d6b7e7882
Yisak Abraham <yisakabraham10@gmail.com>
2026-09-09 09:50:54 +0300

    added files to a folder before sep 9

---
+++ before sep/map/map_learning.cpp
@@ -0,0 +1,13 @@
+// #include <iostream>
+// #include <map>
+// #include <string>
+
+
+// using namespace std;
+
+// int main (){
+//     map <int, string> m1 = {{1, "Geeks"} , {2,"For"}, {3, "Geeks"}};
+    
+//     m1.at(4)="By";
+
+//     cout <
---
+++ before sep/map/yesehak_test.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// #include<vector>
+// #include<string>
+
+// using namespace std;
+
+
+// int main (){
+//     int N;
+//     cin>> N;
+
+//     vector<char> row(N);
+
+//     for (int i = 0; i < N; i++){
+//         row[i] = ;
+//     }
+
+
+
+// }
